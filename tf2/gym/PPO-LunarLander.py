955bfc50b9f36b4fd0295ef985dece049ac678fd
hongjun7 <hongjun-7@daum.net>
2021-02-06 13:42:08 +0900

    fix ppo.

--- tf2/gym/PPO-LunarLander.py
+++ tf2/gym/PPO-LunarLander.py
@@ -1,33 +1,48 @@
 import gym
+import tensorflow as tf
 from tf2.model import PPO
+
+for gpu in tf.config.experimental.list_physical_devices("GPU"):
+    tf.config.experimental.set_memory_growth(gpu, True)
 
 env = gym.make('LunarLander-v2').unwrapped
 env.seed(1)
 
-episode_length = 1000
+log_interval = 20
+episode_length = 50000
+update_timestep = 2000
 
 agent = PPO.Model(obs_dim=env.observation_space.shape[0], act_dim=env.action_space.n,
-                  lr=0.001, gamma=0.99, clip_range=0.2, update_ep_epochs=4)
+                  lr=5e-4, gamma=0.99, clip_range=0.2, update_ep_epochs=4, vf_coef=0.5, ent_coef=1e-4)
 
-for episode in range(episode_length):
+step, running_reward, sum_episode_length = 0, 0, 0
+
+for episode in range(1, episode_length+1):
     obs_cur = env.reset()
-    episode_reward = 0
-    
-    step = 0
+    ep_length = 0
     while True:
         step += 1
+        ep_length += 1
         action, logprob = agent.step(obs_cur)
         obs_nxt, reward, ep_done, _ = env.step(action)
-        if episode % 50 == 0:
-            env.render()
         
-        agent.memory.store(obs_cur, action, reward, logprob)
+        agent.memory.store(obs_cur, action, reward, ep_done, logprob)
 
         obs_cur = obs_nxt
-        episode_reward += reward
-
-        if ep_done or step > 1000:
+        running_reward += reward
+        
+        if step % update_timestep == 0:
             agent.learn()
-            print('episode: %i' % episode, ", step: %i" % step, ", reward: %i" % episode_reward)
+            step = 0
+        
+        if ep_done or ep_length >= 300:
             break
+    sum_episode_length += ep_length
+    if episode % log_interval == 0:
+        avg_length, running_reward = int(sum_episode_length / log_interval), int(running_reward / log_interval)
+        print('Ep {} \t avg. length: {} \t avg. reward: {}'.format(episode, avg_length, running_reward))
+        if running_reward >= 200:
+            print('Solved LunarLander!')
+            break
+        sum_episode_length, running_reward = 0, 0
 env.close()
--- tf2/model/PPO.py
+++ tf2/model/PPO.py
@@ -2,22 +2,23 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow_probability.python.distributions import Categorical
-from tensorflow.keras import layers, optimizers
+from tensorflow.keras import layers, optimizers, initializers
 
 
 class Memory:
     def __init__(self):
-        self.ep_obs, self.ep_act, self.ep_rwd = [], [], []
+        self.ep_obs, self.ep_act, self.ep_rwd, self.ep_done = [], [], [], []
         self.ep_logprob = []
 
-    def store(self, obs, act, rwd, logprob):
+    def store(self, obs, act, rwd, done, logprob):
         self.ep_obs.append(obs)
         self.ep_act.append(act)
         self.ep_rwd.append(rwd)
+        self.ep_done.append(done)
         self.ep_logprob.append(logprob)
 
     def reset(self):
-        self.ep_obs, self.ep_act, self.ep_rwd = [], [], []
+        self.ep_obs, self.ep_act, self.ep_rwd, self.ep_done = [], [], [], []
         self.ep_logprob = []
 
 
@@ -29,16 +30,16 @@ class ActorCritic(tf.Module):
         self.actor = keras.Sequential(
             [
                 layers.Dense(obs_dim),
-                layers.Dense(64, activation=layers.LeakyReLU(), kernel_initializer=keras.initializers.he_uniform()),
-                layers.Dense(64, activation=layers.LeakyReLU(), kernel_initializer=keras.initializers.he_uniform()),
-                layers.Dense(act_dim, activation=layers.LeakyReLU())
+                layers.Dense(32, activation="tanh", kernel_initializer=initializers.he_uniform()),
+                layers.Dense(32, activation="tanh", kernel_initializer=initializers.he_uniform()),
+                layers.Dense(act_dim, activation="softmax")
             ]
         )
         self.critic = keras.Sequential(
             [
                 layers.Dense(obs_dim),
-                layers.Dense(64, activation=layers.LeakyReLU(), kernel_initializer=keras.initializers.he_uniform()),
-                layers.Dense(64, activation=layers.LeakyReLU(), kernel_initializer=keras.initializers.he_uniform()),
+                layers.Dense(32, activation="tanh", kernel_initializer=initializers.he_uniform()),
+                layers.Dense(32, activation="tanh", kernel_initializer=initializers.he_uniform()),
                 layers.Dense(1)
             ]
         )
@@ -47,14 +48,14 @@ class ActorCritic(tf.Module):
     def step(self, obs):
         if obs.ndim < 2:
             obs = obs[np.newaxis, :]
-        action_logits = self.actor(obs)
-        dist = Categorical(logits=action_logits)
+        action_probs = self.actor(obs)
+        dist = Categorical(probs=action_probs)
         action = dist.sample()
         return action.numpy()[0], dist.log_prob(action)
 
     def infer(self, obs, act):
-        action_logits = self.actor(obs)
-        dist = Categorical(logits=action_logits)
+        action_probs = self.actor(obs)
+        dist = Categorical(probs=action_probs)
         action_logprobs = dist.log_prob(act)
         dist_entropy = dist.entropy()
         q_value = self.critic(obs)
@@ -62,13 +63,15 @@ class ActorCritic(tf.Module):
 
 
 class Model(object):
-    def __init__(self, obs_dim, act_dim, lr, gamma, clip_range, update_ep_epochs):
+    def __init__(self, obs_dim, act_dim, lr, gamma, clip_range, update_ep_epochs, vf_coef=0.5, ent_coef=0.001):
         self.obs_dim = obs_dim
         self.act_dim = act_dim
         self.gamma = gamma
         self.clip_range = clip_range
         self.MseLoss = keras.losses.mean_squared_error
         self.update_ep_epochs = update_ep_epochs
+        self.vf_coef = vf_coef
+        self.ent_coef = ent_coef
 
         self.policy = ActorCritic(obs_dim=obs_dim, act_dim=act_dim, lr=lr)
         self.memory = Memory()
@@ -80,29 +83,37 @@ class Model(object):
         old_obs = np.vstack(self.memory.ep_obs)
         old_act = np.array(self.memory.ep_act)
         old_logprob = np.array(self.memory.ep_logprob).ravel()
+
+        ep_reward = np.array(self.memory.ep_rwd)
+        ep_done = np.array(self.memory.ep_done)
         
         rwd = []
         discounted_rwd = 0
-        for reward in reversed(self.memory.ep_rwd):
+        for reward, done in zip(reversed(ep_reward), reversed(ep_done)):
+            if done:
+                discounted_rwd = 0
             discounted_rwd = reward + (self.gamma*discounted_rwd)
             rwd.insert(0, discounted_rwd)
         rwd = tf.constant(rwd, dtype=tf.float32)
-        rwd = (rwd - tf.reduce_mean(rwd)) / tf.math.reduce_std(rwd)
 
         for epoch in range(self.update_ep_epochs):
             with tf.GradientTape() as tape:
                 logprobs, q_value, entropy = self.policy.infer(old_obs, old_act)
-                ratio = tf.exp(logprobs - tf.stop_gradient(old_logprob))
-                advantages = (rwd - tf.stop_gradient(q_value)).numpy()
-                advantages = (advantages - np.mean(advantages)) / np.std(advantages)
-                advantages = tf.constant(advantages, dtype=tf.float32)
+                ratio = tf.exp(logprobs - old_logprob)
+                advs = rwd - q_value
+                advs = (advs - tf.reduce_mean(advs)) / (tf.math.reduce_std(advs) + 1e-6)
                 
                 # surrogate losses
-                L1 = ratio * advantages
-                L2 = tf.clip_by_value(ratio, 1-self.clip_range, 1+self.clip_range) * advantages
+                L1 = ratio * (-advs)
+                L2 = tf.clip_by_value(ratio, 1-self.clip_range, 1+self.clip_range) * (-advs)
                 
-                loss = -tf.minimum(L1, L2) + 0.5*self.MseLoss(q_value, rwd) - 0.001*entropy
-                gradient = tape.gradient(tf.reduce_mean(loss), self.policy.trainable_variables)
+                loss_CLIP = tf.maximum(L1, L2)
+                loss_VF = self.vf_coef * self.MseLoss(q_value, rwd)
+                loss_ENT = self.ent_coef * (-entropy)
+                
+                loss = loss_CLIP + loss_VF + loss_ENT
+                gradient = tape.gradient(loss, self.policy.trainable_variables)
+                gradient, _ = tf.clip_by_global_norm(gradient, 0.5)
                 self.policy.optimizer.apply_gradients(zip(gradient, self.policy.trainable_variables))
 
         self.memory.reset()
